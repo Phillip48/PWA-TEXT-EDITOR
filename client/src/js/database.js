619b85736bd81b45be3159c3363961747943347e
Phillip48 <phillippereira02@gmail.com>
2022-05-25 14:45:32 -0400

    Updated database to have a putDb

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,23 +15,12 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 // export const putDb = async (content) => console.error('putDb not implemented');
 
-// Export a function we will use to POST to the database.
-export const postDb = async (name, home, cell, email)  => {
-  console.log('Post to the database');
-
-  // Create a connection to the database database and version we want to use.
+export const putDb = async (content) => {
+  console.log('PUT to the database');
   const jateDb = await openDB('jate', 1);
-
-  // Create a new transaction and specify the database and data privileges.
   const tx = jateDb.transaction('jate', 'readwrite');
-
-  // Open up the desired object store.
   const store = tx.objectStore('jate');
-
-  // Use the .add() method on the store and pass in the content.
-  const request = store.add({ name: name, home_phone: home, cell_phone: cell, email: email });
-
-  // Get confirmation of the request.
+  const request = store.put({content});
   const result = await request;
   console.log('🚀 - data saved to the database', result);
 };
